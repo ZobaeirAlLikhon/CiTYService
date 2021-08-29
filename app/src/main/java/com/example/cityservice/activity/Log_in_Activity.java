2d7ab2bdfd8117d1d854a358930a77671bad0510
ZobaeirAlLikhon <ZobaeirAlLikhon@gmail.com>
2021-08-29 12:54:42 +0600

    Log in page design complited

---
+++ app/src/main/java/com/example/cityservice/activity/Log_in_Activity.java
@@ -0,0 +1,16 @@
+package com.example.cityservice.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cityservice.R;
+
+public class Log_in_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
---
+++ app/src/main/java/com/example/cityservice/activity/Sign_up_Activity.java
@@ -0,0 +1,16 @@
+package com.example.cityservice.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cityservice.R;
+
+public class Sign_up_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
