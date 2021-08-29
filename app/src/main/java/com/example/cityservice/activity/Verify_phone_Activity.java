425671ac66a38461aa758810e2db6963f685f446
ZobaeirAlLikhon <ZobaeirAlLikhon@gmail.com>
2021-08-29 22:08:54 +0600

    Sign Up page design complited

---
+++ app/src/main/java/com/example/cityservice/activity/Verify_phone_Activity.java
@@ -0,0 +1,16 @@
+package com.example.cityservice.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cityservice.R;
+
+public class Verify_phone_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+    }
+}
